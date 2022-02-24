b0b11dc56b2d6802ff5adb386e35adfe9603bebe
David Kerns <david@kerns.dk>
2022-02-24 22:44:46 +0100

    Removed old config files

--- config/knock-knock.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use craft\helpers\App;
-
-return [
-	'enabled' => (bool) App::env('KNOCK_KNOCK_IS_ENABLED'),
-	'password' => App::env('KNOCK_KNOCK_PASSWORD'),
-];
--- config/seomatic.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use craft\helpers\App;
-
-return [
-    'pluginName' => 'SEO',
-    'environment' => (bool) App::env('SEOMATIC_IS_LIVE') ? 'live' : 'local',
-];
